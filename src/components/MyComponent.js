2599c9dbb1bdacce3fee160aa4ca1e2d63a0ac91
Abel Pérez <abelper54@gmail.com>
2023-02-16 19:27:40 -0400

    Update MyComponent.js

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function MyComponent() {
-  return <h1>Hello World!</h1>;
+export default function MyComponent({ greeting = 'Hello', guests = [] }) {
+  console.log(guests);
+  return (
+    <>
+      <h1>
+        {greeting} {guests.join(',')}
+      </h1>
+    </>
+  );
 }
+
+MyComponent.propTypes = {
+  /** The greeting to be rendered */
+  greeting: PropTypes.string,
+  /** An array of guests names */
+  guests: PropTypes.arrayOf(PropTypes.string),
+};
+
+MyComponent.defaultProps = {
+  greeting: 'Hello',
+  guests: [],
+};
